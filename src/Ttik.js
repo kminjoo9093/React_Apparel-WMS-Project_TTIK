964483357104789bf4ac8d7e4786539ed0a5a132
mouse97419 <mouse97419@gmail.com>
2026-01-05 14:47:14 +0900

    JY partnerRegister-01-05 14:47

--- src/Ttik.js
+++ src/Ttik.js
@@ -1,9 +1,14 @@
-import serverUrl from "./db/server.json";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import PartnerRegister from "./pages/partner/PartnerRegister";
 
 function Ttik() {
 
   return (
     <div className="Ttik">
+      <Routes>
+        <Route path="/partner" element={ <PartnerRegister /> } />
+      </Routes>
     </div>
   );
 }
