87f3017ddea7a497b20b8975e611c10756e1c342
minjoo <minjoook99@gmail.com>
2026-01-07 21:24:59 +0900

    mj-상품등록 반응형 ui

--- src/pages/product/ProductBarcode.js
+++ src/pages/product/ProductBarcode.js
@@ -4,7 +4,7 @@ import styleRegister from "../../css/ProductRegister.module.css";
 function ProductBarcode(){
     return (
         <div className={styleProdModal.modalInner}>
-        <p>상품 바코드를 자동 생성합니다.</p>
+        <p>상품 QR코드를 자동 생성합니다.</p>
         <div className={styleProdModal.modalContents}>
             <div>
                 <input className={styleProdModal.year} type="text"></input>
